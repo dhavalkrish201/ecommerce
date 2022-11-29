ed5912ae2dd63fa1d64375ad37c30194f5718f16
Dhaval Shah <dhaval.shah@krishtechnolabs.com>
2022-11-29 19:23:04 +0530

    Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,11 +8,23 @@ const Product = () => {
       const res = await fetch("https://fakestoreapi.com/products");
       const data = await res.json();
       console.log("data", data);
+      setProduct(data);
     };
     fetchProduct();
   }, []);
 
-  return <div>Product</div>;
+  return (
+    <div className="productsWrapper">
+      {product.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt="product img" />
+          <h4>{product.title}</h4>
+          <h5>{product.price}</h5>
+          <button className="btn"> Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Product;
